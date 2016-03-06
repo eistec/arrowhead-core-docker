cd36e4b0c78b3165b0e1e5e4de795a09e39e4fcf
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-03-06 11:08:26 +0100

    rename tag armv7 -> armhf

--- build-all-images.sh
+++ build-all-images.sh
@@ -10,5 +10,5 @@ done
 
 # Build for ARM (requires binfmt-misc support and a qemu-arm-wrapper)
 for d in ${DIRS}; do
-    (cd "${d}" && docker build -t "${IMAGE_TAG_PREFIX}${d}:armv7" -f Dockerfile.armv7 . && docker push "${IMAGE_TAG_PREFIX}${d}:armv7")
+    (cd "${d}" && docker build -t "${IMAGE_TAG_PREFIX}${d}:armhf" -f Dockerfile.armhf . && docker push "${IMAGE_TAG_PREFIX}${d}:armhf")
 done
